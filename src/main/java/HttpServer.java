204d925b4ab5f43ece4d321998fe0ae948884569
pashna <p02p@yandex.ru>
2014-09-10 00:46:28 +0400

    many tests

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -43,16 +43,16 @@ public class HttpServer {
 
         public void run() {
             try {
-                readInputHeaders();
+                readRequest();
                 writeResponse();
             } catch (Throwable t) {
-                /*do nothing*/
+
             } finally {
                 try {
                     s.close();
                 } catch (Throwable t) {
                     t.printStackTrace();
-                    /*do nothing*/
+
                 }
             }
             System.err.println("Client processing finished");
@@ -60,7 +60,6 @@ public class HttpServer {
 
         private void writeResponse() throws Throwable {
             byte [] byteArray=null;
-            //File file = new File(Config.dir + "/" + requestMap.get("FILE"));
             if (requestMap.get("METHOD").equals("GET")) {
                 Path path = Paths.get(Config.dir, requestMap.get("FILE"));
                 try {
@@ -70,20 +69,16 @@ public class HttpServer {
                 } catch (IOException e) { // exception NoFile
                     writeHeader(404, -1L);
                     byteArray = "PAGE NOT FOUND".getBytes();
-                    //response = "HTTP/1.1 404 NOT FOUND\r\n\r\nNOT FOUND PAGE";
                 }
             }
             else {
                 writeHeader(405, -1L);
-                //response = "405 Method Not Allowed\r\n\r\nAllow: GET\r\n";
             }
-            //os.write(response.getBytes());
-            //System.out.println();
             os.write(byteArray);
             os.flush();
         }
 
-        private void readInputHeaders() throws Throwable {
+        private void readRequest() throws Throwable {
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
             String s="";
             String buffer;
@@ -103,12 +98,15 @@ public class HttpServer {
         private void parseRequestToMap(String requestString) {
             if (requestString.substring(0,3).equals("GET")) {
                 requestMap.put("METHOD", "GET");
-                String filePath = requestString.substring(4, requestString.indexOf(" ", 4));
+                String filePath = requestString.substring(4, requestString.indexOf(" ", 4)); // Подстрока, начиная с пробела после Get
+                                                                                            // И до второго пробела
                 try {
                     filePath = java.net.URLDecoder.decode(filePath, "UTF-8");
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
+                if (filePath.contains("../")) filePath = "randomUnnownFile123412"; // Если идет вверх по дереву - ставим заранее неизвестный файл
+                if (filePath.contains("?")) filePath = filePath.substring(0, filePath.indexOf("?")); // Если есть строка подзапроса удаляем ее
                 requestMap.put("FILE", filePath);
                 if (filePath.endsWith("/")) requestMap.put("FILE", filePath+"index.html");
 
@@ -127,21 +125,15 @@ public class HttpServer {
         private void writeHeader(int status, long length) {
             String response = "HTTP/1.1 " + HttpStatusHelper.get(status);
             if (status == 200) {
-            /*    response +="\r\nDate: " + getTime()
-                        + "\r\nServer: myBeautyServer v.1.0";
-                response += getContentType(requestMap.get("FILE"));
-                response +="\r\nContent-Length: " + length;
-                response +="\r\nConnection: close\r\n\r\n";*/
                 response += "\r\nDate: " + getTime() +
                         "\r\nServer: myBeautyServer v.1.0" +
                         "\r\n" + getContentType(requestMap.get("FILE")) +
                         "\r\nContent-Length: " + length +
                         "\r\nConnection: close";
-                //
 
             } else if (status == 404) {
-                System.out.println("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-            } else {// Статус 403
+
+            } else {
 
             }
             response += "\r\n\r\n";
